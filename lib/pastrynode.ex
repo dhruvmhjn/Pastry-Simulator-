e77df8693592b73a070c6896c527da59dd5f74c4
blackthorn39 <blackthorn_39@live.in>
2017-10-20 17:01:56 -0400

    init pastry node

--- lib/pastrynode.ex
+++ lib/pastrynode.ex
@@ -1,4 +1,12 @@
 defmodule PastryNode do
     use GenServer
+    def start_link(x,numRequests) do
+    b=4
+    input_srt = Integer.to_string(x)
+    nodeid = String.slice(Base.encode16(:crypto.hash(:sha256, input_srt)),32,32)
+    GenServer.start_link(__MODULE__, {nodeid,b,numRequests}, name: String.to_atom(nodeid))    
+    end
+
+    
 
 end
