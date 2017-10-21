3164fed738910f9df61e1eed3a51601d4d0defea
blackthorn39 <blackthorn_39@live.in>
2017-10-21 00:27:39 -0400

    route

--- lib/pastrynode.ex
+++ lib/pastrynode.ex
@@ -4,26 +4,30 @@ defmodule PastryNode do
     b=4
     input_srt = Integer.to_string(x)
     nodeid = String.slice(Base.encode16(:crypto.hash(:sha256, input_srt)),32,32)
-    GenServer.start_link(__MODULE__, {nodeid,b,nodes,numRequests}, name: String.to_atom(nodeid))    
+    GenServer.start_link(__MODULE__, {nodeid,b,nodes,numRequests}, name: String.to_atom("n#{nodeid}"))    
     end
 
     def init({nodeid,b,nodes,numRequests}) do
-<<<<<<< HEAD
         {:ok, {nodeid,[],{},numRequests}}
     end
 
+   
     def handle_cast({:route,key},{nodeid,leaf,routetable,req})do
-        #compute difference between self and key
-
-
+        #compute difference between leafset and key
+        {keyval,_} = Integer.parse(key,16)
 
         #check leaf set
+        {firstlist,_} = Integer.parse(to_string(List.first(leaf)),16)
+        {lastleaf,_} = Integer.parse(to_string(List.last(leaf)),16)
+
+        if(keyval >= firstleaf) &&(keyval <= lastleaf) do
+            
+        end
+        
+
 
         
         {:noreply,{nodeid,leaf,routetable,req}}
-=======
-        {:ok, {nodeid,{},{},numRequests}}
->>>>>>> c1b6c1f8684884af26947e4a8fadd87fc7276cc2
     end
 
 end
