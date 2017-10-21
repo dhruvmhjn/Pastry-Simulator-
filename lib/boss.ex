e2e81b9696f91f9ed8ba68f76eb21cf031002375
Dhruv Mahajan <dhruvmhjn@gmail.com>
2017-10-21 15:26:11 -0400

    ABC

--- lib/boss.ex
+++ lib/boss.ex
@@ -18,23 +18,14 @@ defmodule Boss do
             
     def boss_receiver(numNodes,numRequests,a) do
         receive do
-            {:rumourpropogated,b} ->
-                IO.puts "Time in MilliSeconds: #{b-a}"
-                :init.stop
+            
             {:nodes_created} ->
-                #rstring = "This is the first rumour"
+                
                 IO.puts "Nodes created, netwoek init started"
-                
-                # n_list = Enum.to_list 1..numNodes
-                # nodeid_list = Enum.map(n_list, fn(x) -> String.slice(Base.encode16(:crypto.hash(:sha256, Integer.to_string(x) ) ),32,32) end)
-                # IO.inspect ( Enum.at(nodeid_list,0))
-                
                 nextnode = "n"<>String.slice(Base.encode16(:crypto.hash(:sha256, Integer.to_string(1) ) ),32,32)
                 # ADD INIT NEXT cast here 
                 GenServer.cast(String.to_atom(nextnode),{:intialize_table_first})
 
-                
-                #rstring = "This is the first rumour"
 
             {:network_ring_created} ->
                 IO.puts "Network ring creattion msg recieved."
@@ -45,6 +36,7 @@ defmodule Boss do
                 #CAST initial message to everyone 
                 
                 Enum.map(nodeid_list, fn(x) -> GenServer.cast(String.to_atom(x),{:create_n_requests}) end)
+
                 #a = System.system_time(:millisecond)
                 
             {:sumcomputed,b} ->
