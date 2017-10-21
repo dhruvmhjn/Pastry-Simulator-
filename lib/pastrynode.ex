65db391bd8430cc1e887fa8ac1edb2c98b4a5537
blackthorn39 <blackthorn_39@live.in>
2017-10-21 15:27:26 -0400

    routing

--- lib/pastrynode.ex
+++ lib/pastrynode.ex
@@ -7,9 +7,9 @@ defmodule PastryNode do
     GenServer.start_link(__MODULE__, {nodeid,b,nodes,numRequests}, name: String.to_atom("n#{nodeid}"))    
     end
 
-    def init({nodeid,b,nodes,numRequests}) do
-        
-        {:ok, {nodeid,[],[],numRequests}}
+    def init({nodeid,b,nodes,numRequests}) do        
+        routetable = Matrix.from_list([[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]])
+        {:ok, {nodeid,[],routetable,numRequests}}
     end
 
     def handle_cast({:intialize_table,hostid},{nodeid,leaf,routetable,req})do
@@ -33,10 +33,12 @@ defmodule PastryNode do
         {firstlist,_} = Integer.parse(List.first(leaf),16)
         {lastleaf,_} = Integer.parse(List.last(leaf),16)
 
-       # if((keyval >= firstleaf) &&(keyval <= lastleaf)) do
+       if((keyval >= firstleaf) &&(keyval <= lastleaf)) do
             #compare abs of differences 
-        #    d = firstleaf - keyval
-        #end
+            min_diff_leaf = Enum.min_by(leaf, fn(x) -> Kernel.abs(x - keyval) end)
+
+            #cast to min_diff_leaf        
+        end
        
 
 
