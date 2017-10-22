3f8be1b293be422ca9cf937dc02fb7099a2a4ecc
Dhruv Mahajan <dhruvmhjn@gmail.com>
2017-10-22 19:30:29 -0400

    Boss and listenr

--- lib/listner.ex
+++ lib/listner.ex
@@ -29,7 +29,7 @@ defmodule Listner do
         if(hop_msgs_recieved <= (numrequests*numnodes)) do
             hop_counter = hop_counter + no_of_hops
         else
-            send(Process.whereis(:boss),{:all_requersts_served,hop_counter})
+            send(Process.whereis(:boss),{:all_requests_served,hop_counter})
         end
     end
 end
